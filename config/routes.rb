6015e5955c45179115720126775e6779ff5d244d
n.l <n.leteurtre@gmail.com>
2020-11-17 15:40:36 +0100

    add rails task managerexercize

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "tasks/new", to: "tasks#new", as: :new_task
+
+  # Afficher la liste des tâches
+  get "tasks", to: "tasks#index"
+
+  # Afficher une tâche
+  get "tasks/:id", to: "tasks#show", as: "task"
+  post "tasks", to: "tasks#create"
+
+  # Editer une tâche
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete a restaurant
+  delete "tasks/:id", to: "tasks#destroy"
 end
